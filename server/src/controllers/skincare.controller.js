89498aa458111bdb65647b99b7b57b7a018947b4
Anmolroan <anmol.kumar@masaischool.com>
2022-06-14 23:16:55 +0530

    skincare schema is done

--- server/src/controllers/skincare.controller.js
+++ server/src/controllers/skincare.controller.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const Facewash=require("../models/faceWash.model");
+const Skincare=require("../models/skincare.model");
 const router =express.Router();
 router.get('/', async function(req, res){
     try{
-        const facewashes = await Facewash.find().lean().exec();
-        console.log(facewashes.length)
-        return res.status(201).send(facewashes)
+        const skincares = await Skincare.find().lean().exec();
+        console.log(skincares.length)
+        return res.status(201).send(skincares)
     }catch(err){
         return res.status(500).send(err.message)
     }
 })
 router.get('/:id', async function(req, res){
     try{
-        const facewash =await Facewash.findById(req.params.id).lean().exec();
-        return res.status(201).send(facewash)
+        const skincare =await Skincare.findById(req.params.id).lean().exec();
+        return res.status(201).send(skincare)
     }catch(err){
         return res.status(500).send(err.message)
     }
 })
 router.post('/', async function(req, res){
     try{
-        const facewash = await Facewash.create(req.body);
-        return res.status(200).send(facewash)
+        const skincare = await Skincare.create(req.body);
+        return res.status(200).send(skincare)
     }catch(err){
         res.status(500).send(err.message)
     }
--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const facewashController =require('./controllers/facewash.controller');
+const skincareController = require('./controllers/skincare.controller')
 app.use("/facewash",facewashController);
+app.use("/skincare",skincareController);
 module.exports =app;
