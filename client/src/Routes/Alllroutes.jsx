f920bd31dc5857cce858be145eba114af0d3e4f5
Aakash Kushwaha <99542364+Aakash-kushwaha@users.noreply.github.com>
2022-06-14 12:32:59 +0530

    Description_page_done(yet to be linked)

--- client/src/Routes/Alllroutes.jsx
+++ client/src/Routes/Alllroutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Products from '../Pages/Products'
 import Navbar from '../components/Navbar'
+import Product from '../Pages/Product'
 
 const Alllroutes = () => {
   return (
@@ -9,6 +10,7 @@ const Alllroutes = () => {
         <Navbar></Navbar>
         <Routes>
           <Route path="/products/:id" element={<Products></Products>}></Route>
+           <Route path='/products/:id/:description' element={<Product></Product>}></Route>
         </Routes>
      
     </div>
